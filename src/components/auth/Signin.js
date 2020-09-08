e2b3d48f2dd2278220e3362ec0d0f5d25905ccdd
Eunit <eunithelp@gmail.com>
2020-09-08 21:39:02 +0100

    Update Signin.js
    * Transformed Material UI into Bootstrap CSS

--- src/components/auth/Signin.js
+++ src/components/auth/Signin.js
@@ -1,45 +1,12 @@
-import React, {useState} from 'react'
-import Card from '@material-ui/core/Card'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import Button from '@material-ui/core/Button'
-import TextField from '@material-ui/core/TextField'
-import Typography from '@material-ui/core/Typography'
-import Icon from '@material-ui/core/Icon'
-import { makeStyles } from '@material-ui/core/styles'
+import React from 'react';
 import auth from './../auth/auth-helper'
 import {Redirect} from 'react-router-dom'
 import {signin} from './api-auth.js'
 
-const useStyles = makeStyles(theme => ({
-  card: {
-    maxWidth: 600,
-    margin: 'auto',
-    textAlign: 'center',
-    marginTop: theme.spacing(5),
-    paddingBottom: theme.spacing(2)
-  },
-  error: {
-    verticalAlign: 'middle'
-  },
-  title: {
-    marginTop: theme.spacing(2),
-    color: theme.palette.openTitle
-  },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
-    width: 300
-  },
-  submit: {
-    margin: 'auto',
-    marginBottom: theme.spacing(2)
-  }
-}))
+export default Signin;
 
-export default function Signin(props) {
-  const classes = useStyles()
-  const [values, setValues] = useState({
+function Signin(props) {
+  const [values, setValues] = React.useState({
       email: '',
       password: '',
       error: '',
@@ -57,7 +24,7 @@ export default function Signin(props) {
         setValues({ ...values, error: data.error})
       } else {
         auth.authenticate(data, () => {
-          setValues({ ...values, error: '',redirectToReferrer: true})
+          setValues({ ...values, error: '', redirectToReferrer: true})
         })
       }
     })
@@ -77,24 +44,126 @@ export default function Signin(props) {
       return (<Redirect to={from}/>)
   }
 
-  return (
-      <Card className={classes.card}>
-        <CardContent>
-          <Typography variant="h6" className={classes.title}>
-            Sign In
-          </Typography>
-          <TextField id="email" type="email" label="Email" className={classes.textField} value={values.email} onChange={handleChange('email')} margin="normal"/><br/>
-          <TextField id="password" type="password" label="Password" className={classes.textField} value={values.password} onChange={handleChange('password')} margin="normal"/>
-          <br/> {
-            values.error && (<Typography component="p" color="error">
-              <Icon color="error" className={classes.error}>error</Icon>
-              {values.error}
-            </Typography>)
-          }
-        </CardContent>
-        <CardActions>
-          <Button color="primary" variant="contained" onClick={clickSubmit} className={classes.submit}>Submit</Button>
-        </CardActions>
-      </Card>
-    )
-}
+
+    return (
+    <div className="">
+        <div className="container">
+            <div className="auth-container rounded w-100 h-100 mt-12vh">
+                <div className="row">
+                    {/* left part STARTS */}
+                    <div className="d-none d-sm-none d-md-none d-lg-block col-lg-5 p-0">
+                        <div className="left-part">
+                            <div className="logo-area">
+                                <div className="row">
+                                    <div className="col-lg-8">
+                                        <img className="img-fluid ml-2 mt-1 float-left text-left align-top"
+                                            src="img/logo/300x80.png" alt="Fumana logo" />
+                                    </div>
+                                        <div className="col-lg-4">
+                                            {/* We need this part to make it responsive */}
+                                            <div className="d-none d-sm-none d-md-none d-lg-none"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="place-holder-img justify-content-center align-content-center text-center">
+                                    <img className="img-fluid" src="img/logo/300x400.png" alt="Fumana" />
+                            </div>
+                                </div>
+                            </div>
+                            {/* right part STARTS */}
+                            <div className="col-12 col-md-12 col-lg-7 p-0">
+                                <div className="right-part">
+                                    <div className="row">
+                                        <div className="col-5 col-md-5">
+                                            {/* We need this part to make it responsive */}
+                                            <div className="d-none d-sm-none d-md-none d-lg-none"></div>
+                                        </div>
+                                        <div className="col-12 col-md-7">
+                                            <div className="sign-up-area float-right mx-5 mx-md-3">
+                                                To join Fumana? &nbsp;<a
+                                                    className="font-weight-bold color-primary font-size-normal text-decoration-none"
+                                                    title="Sign up" href="signup.html">Sign Up here</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="row">
+                                        <div className="col-1 col-md-3">
+                                            {/* We need this part to make it responsive */}
+                                            <div className="d-none d-sm-none d-md-none d-lg-none"></div>
+                                        </div>
+                                        <div className="col-10 col-md-6">
+                                            <div className="pt-8vh input-field-container justify-content-center">
+                                                <div className="auth-title font-weight-bold">
+                                                    <div className="color-gray">Already have an account?</div>
+                                                    <div className="font-weight-bold py-2 text-dark">Sign in here</div>
+                                                </div>
+                                                <form>
+                                                    <div className="form-group pt-3">
+                                                        <label className="pb-2 color-gray font-weight-bold"
+                                                            for="inputemail1">Email</label>
+                                                        <input id="email" type="email" label="Email" value={values.email}
+                                                            onChange={handleChange('email')} className="form-control"
+                                                            placeholder="Enter your email address" name="email" />
+                                                    </div>
+                                                    <div className="form-group">
+                                                        <label className="pb-2 color-gray font-weight-bold"
+                                                            for="inputPassword1">Password</label>
+                                                        <input type="password" className="form-control"
+                                                            placeholder="Password" name="password" id="password" type="password"
+                                                            label="Password" value={values.password} onChange={handleChange('password')} />
+                                                    </div>
+                                                    <div className="checkbox">
+                                                        <label className="color-gray font-weight-bold">
+                                                            <input type="checkbox" name="checkbox" /> Stay logged in
+                                                </label>
+                                            </div>
+                                            {/* <div className="font-weight-bold">
+                                                        {values.error && (error)}
+                                                    {values.error}
+                                            </div>
+                                            */}
+                                            <button onClick={clickSubmit} type="submit"
+                                                className="px-5 mt-5 btn btn-default bg-primary color-white font-weight-bold">Log
+                                                in</button>
+                                        </form>
+                                    </div>
+                                                <div className="login-trouble my-4">
+                                                    <a href="forgot-pass.html" className="text-decoration-none color-primary"
+                                                        title="Trouble logging in?"> Can't access your account?</a>
+                                                </div>
+                                                <div className="auth-ending mt-2">
+                                                    <div className="d-inline">
+                                                        <span>
+                                                            <a className="color-primary text-decoration-none" title="Contact us"
+                                                                href="contact.html">Contact
+                                                    us</a>
+                                                        </span>
+                                                        <span>
+                                                            |
+                                            </span>
+                                                        <span>
+                                                            <a className="color-primary text-decoration-none ml-2"
+                                                                title="Privacy policy" href="privacy.html">Privacy
+                                                    policy</a>
+                                                        </span>
+                                                    </div>
+                                                    <div className="copyright-info color-gray my-2">
+                                                        <small>Copyright &copy; <span id="copyrightYear"></span> Fumana. All
+                                                rights reserved
+                                            </small>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="col-1 col-md-3">
+                                                {/* We need this part to make it responsive */}
+                                                <div className="d-none d-sm-none d-md-none d-lg-none"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        );
+    }
