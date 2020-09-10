6f9e5b4252917df441d5b69db6f157457bdc56f5
Eunit <eunithelp@gmail.com>
2020-09-10 18:38:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import './Styles.css';
+import './Sidenav.css';
 import './Theme.css';
 import './Reset.css';
 import './Normalize.css';
 import './Animate.min.css';
 import './Html5reset.css';
 import Signin from './components/auth/Signin';
-import MainRouter from './components/MainRouter'
+import MainRouter from './components/MainRouter';
+import Sidenav from './components/core/Sidenav';
 import {BrowserRouter} from 'react-router-dom'
 import { hot } from 'react-hot-loader'
 
@@ -20,6 +22,7 @@ const App = () => {
   }, [])
   return (
   <BrowserRouter>
+      <Sidenav />
       <Signin />
       <MainRouter/>
   </BrowserRouter>
