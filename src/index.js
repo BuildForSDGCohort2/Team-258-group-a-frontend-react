9a85ef525a50daaf1f3f22adbc30c56ca2decd77
Eunit <eunithelp@gmail.com>
2020-09-13 16:15:59 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './App.css';
+import './components/assets/styles/Styles.css';
+import './components/assets/styles/Sidenav.css';
+import './components/assets/styles/Theme.css';
+import './components/assets/styles/Reset.css';
+import './components/assets/styles/Normalize.css';
+import './components/assets/styles/Animate.css';
+import './components/assets/styles/Html5reset.css';
+import Signin from './components/auth/Signin';
+import MainRouter from './components/MainRouter';
+import Sidenav from './components/core/Sidenav';
+import {BrowserRouter} from 'react-router-dom'
+import { hot } from 'react-hot-loader'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles)
+    }
+  }, [])
+  return (
+  <BrowserRouter>
+      <Sidenav />
+      <Signin />
+      <MainRouter/>
+  </BrowserRouter>
+)}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default hot(module)(App)
+
