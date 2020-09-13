5e14307b5b27b38d76ca54eece8d2684e90fd5b4
Eunit <eunithelp@gmail.com>
2020-09-13 16:09:43 +0100

    Update Sidenav.js

--- src/components/core/Sidenav.js
+++ src/components/core/Sidenav.js
@@ -1,55 +1,79 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faDashboard, faBriefcase, faUser, faUsers, faCogs, faLegal } from '@fortawesome/freesolid-svg-icons';
-
+import Home from './Home';
 class Sidenav extends Component {
     render() {
-        const FaDashboard = <FontAwesome icon={["fal", "dashboard"]} />,
-            FaBriefcase = <FontAwesome icon={["fal", "briefcase"]} /> ,
-            FaUser = <FontAwesome icon={["fal", "user"]} />,
-            FaUsers = <FontAwesome icon={["fal", "users"]} />,
-            FaCogs = <FontAwesome icon={["fal", "cogs"]} />,
-            FaLegal = <FontAwesome icon={["fal", "legal"]} />;
-        return (
+            return (
             <div className="wrapper d-flex align-items-stretch">
-                <nav id="sidebar" className="active">
-                    <ul className="list-unstyled components mb-5">
-                        <div className="text-center mb-2">
-                            <li id="sidebarCollapse" className="nav-bars mt-4 ml-auto pl-auto font-size-large"></li>
-                        </div>
-                        <li className="active color_light__accessibility">
-                            <a href="#">{FaDashboard} Dashboard</a>
+            <nav id="sidebar" className="active">
+                <ul className="list-unstyled components mb-5">
+                    <div className="text-center mb-2">
+                        <li id="sidebarCollapse" className="nav-bars mt-4 ml-auto pl-auto font-size-large">
                         </li>
-                        <li className="color_light__accessibility">
-                            <a href="#">{FaBriefcase} Bureau</a>
-                        </li>
-                        <li className="color_light__accessibility">
-                            <a href="#">{FaUser} User</a>
-                        </li>
-                        <li className="color_light__accessibility">
-                            <a href="#">{FaUser} Employer</a>
-                        </li>
-                        <li className="color_light__accessibility">
-                            <a href="#">{faUsers} Job seeker</a>
-                        </li>
-                        <li className="color_light__accessibility">
-                            <a href="#">{faCogs} Settings</a>
-                        </li>
-                        <li className="color_light__accessibility">
-                            <a href="#">{faLegal} Legal</a>
-                        </li>
-                    </ul>
+                    </div>
+                    <li className="active color_light__accessibility">
+                        <a href="#"><span className="fa fa-dashboard"></span> Dashboard</a>
+                    </li>
+                    <li className="color_light__accessibility">
+                        <a href="#"><span className="fa fa-briefcase"></span> Bureau</a>
+                    </li>
+                    <li className="color_light__accessibility">
+                        <a href="#"><span className="fa fa-user"></span> User</a>
+                    </li>
+                    <li className="color_light__accessibility">
+                        <a href="#"><span className="fa fa-user"></span> Employer</a>
+                    </li>
+                    <li className="color_light__accessibility">
+                        <a href="#"><span className="fa fa-users"></span> Job seeker</a>
+                    </li>
+                    <li className="color_light__accessibility">
+                        <a href="#"><span className="fa fa-cogs"></span> Settings</a>
+                    </li>
+                    <li className="color_light__accessibility">
+                        <a href="#"><span className="fa fa-paper-plane"></span> Legal</a>
+                    </li>
+                </ul>
 
-                    <div className="footer">
-                        <p>
-                            Copyright &copy; 2020 Fumana
-                        </p>
+                <div className="footer">
+                    <p>
+                        Copyright &copy; 2020 Fumana
+                    </p>
+                </div>
+            </nav>
+            {/* Page Content  */}
+            <div id="content" className="p-4 p-md-5">
+
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="container-fluid">
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="nav navbar-nav ml-auto">
+                                <li className="nav-item active">
+                                    <a className="nav-link" href="#">Dashboard</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Bureau</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">User</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Job seeker</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Settings</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="#">Legal</a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </nav>
-                {/* Contents goes here */}
-                <h2 class="mb-4">Team 258 Group A</h2>
+                {/* Content goes here */}
+                <h2 className="mb-4">Team 258 Group A</h2>
+                <Home />
             </div>
+        </div>
+
         )
     }
 }
