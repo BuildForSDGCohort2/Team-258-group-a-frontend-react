42c99acd3602d9dda2170bb9bb9b29775fc0079c
leanny01 <dleannykalangu@gmail.com>
2020-09-15 10:29:33 +0200

    added fake data

---
+++ src/components/bureau/index.js
@@ -0,0 +1,79 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Presentation from "./presentation";
+
+const bureauList = [
+  {
+    id: "123",
+    logoImg: "logo_url",
+    name: "Bureau_1",
+    type: "bureau_1_type",
+    business_reg_no: "1234",
+    date_joined: Date.now(),
+    social_fb: "url",
+    social_tweeter: "url",
+    social_linkedin: "url",
+    social_google: "url",
+    contact: {
+      primary_email: "email",
+      secondary_email: "email",
+      phone_number_1: "phone",
+      phone_number_2: "phone",
+      contact_person: "person",
+      contact_position: "position",
+    },
+    address: [
+      {
+        address: "address",
+        address_line_2: "address_line_2",
+        city: "city",
+        province: "province",
+        city: "city",
+        country: "country",
+      },
+    ],
+    description:"description"
+  },
+  {
+    id: "123_2",
+    logoImg: "logo_url_2",
+    name: "Bureau_1_2",
+    type: "bureau_1_type_2",
+    business_reg_no: "1234_2",
+    date_joined: Date.now(),
+    social_fb: "url_2",
+    social_tweeter: "url_2",
+    social_linkedin: "url_2",
+    social_google: "url_2",
+    contact: {
+      primary_email: "email_2",
+      secondary_email: "email_2",
+      phone_number_1: "phone_2",
+      phone_number_2: "phone_2",
+      contact_person: "person_2",
+      contact_position: "position_2",
+    },
+    address: [
+      {
+        address: "address_2",
+        address_line_2: "address_line_2_2",
+        city: "city_2",
+        province: "province_2",
+        city: "city_2",
+        country: "country_2",
+      },
+    ],
+    description:"description_2"
+  },
+
+];
+
+// Get a single ureau
+const getBureauInfo = (id)=>{
+    return bureauList.filter( bureau => bureau.id === id)
+}
+const BureauInfo = () => {
+  const bureauInfo = getBureauInfo(id);
+
+  return <Presentation data={bureauInfo} />;
+};
---
+++ src/components/bureau/presentation.js
@@ -0,0 +1,6 @@
+
+
+const Presentation = ()=>{
+
+    return(<div>Hey there this is the bureau, whatch the space </div>)
+}
