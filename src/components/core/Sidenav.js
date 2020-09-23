f7a386f8fe9834242f20b1ff3bcca89f87e42a08
leanny01 <dleannykalangu@gmail.com>
2020-09-23 08:29:55 +0200

    fixed closing tag bug

--- src/components/core/Sidenav.js
+++ src/components/core/Sidenav.js
@@ -24,7 +24,7 @@ class Sidenav extends Component {
             </li>
             <li className="color_light__accessibility">
               <a href="/user">
-              <a href="#">
+
                 <span className="fa fa-user"></span> User
               </a>
             </li>
