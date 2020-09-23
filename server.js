c3434a08ee666cea42a8be20a0d4fb70179b99ae
leanny01 <dleannykalangu@gmail.com>
2020-09-23 09:14:48 +0200

    fixed heroku deployment issue

---
+++ server.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+//app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+
+ });
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}!`);
+ });
