043860d10442554e2ebc443cc1afc30ff7d120eb
Emmanuel Uchenna <eunitwap@gmail.com>
2020-10-16 09:12:54 +0100

    Added Forgot Password route

--- src/components/MainRouter.js
+++ src/components/MainRouter.js
@@ -3,7 +3,8 @@ import {Route, Switch} from 'react-router-dom'
 import Home from './core/Home'
 import Users from './user/Users'
 import Signup from './user/Signup'
-import Signin from './auth/Signin'
+import Signup from './user/Signup'
+import ForgotPass from './user/ForgotPass'
 import EditProfile from './user/EditProfile'
 import Profile from './user/Profile'
 import PrivateRoute from './auth/PrivateRoute'
@@ -23,6 +24,7 @@ const MainRouter = () => {
         <Route path="/users" component={Users}/>
         <Route path="/signup" component={Signup}/>
         <Route path="/signin" component={Signin}/>
+        <Route path="/forgot-pass" component={ForgotPass}/>
         <PrivateRoute path="/user/edit/:userId" component={EditProfile}/>
         <Route path="/user/:userId" component={Profile}/>
         <Route path='/bureau' component={BureauInfo} />
